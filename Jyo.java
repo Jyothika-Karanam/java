b2297c4d943d734a1a0f007c713e04f30b311c01
Jyothika Karanam <jyothikakaranam991@gmail.com>
2025-07-16 19:28:03 +0530

    Add files via upload

--- Jyo.java
+++ Jyo.java
@@ -1,15 +1,14 @@
-import java.util.Scanner;
-class Jyo{
+class Jyo
+{
 public static void main(String args[]){
-Scanner ob=new Scanner(System.in);
-int n,rev=0;
-System.out.println("enter n");
-n=ob.nextInt();
-while(n>0){
-int digit=n%10;
- rev=rev*10+digit;
-n=n/10;
+int arr[]={1,2,3,4,5,6,7,8,9};
+int count=0;
+for(int i=0;i<arr.length;i++){
+if(arr[i]%3==0){
+count++;
+System.out.println(arr[i]);
 }
-System.out.println("the reverse number"+rev);
+}
+System.out.println(count);
 }
 }
