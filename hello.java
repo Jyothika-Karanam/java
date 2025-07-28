27533bacdd6de26ec018c3dd8d95f03d9a7d5ee7
Jyothika Karanam <jyothikakaranam991@gmail.com>
2025-07-28 21:31:54 +0530

    Add files via upload

---
+++ hello.java
@@ -0,0 +1,10 @@
+import java.util.*;
+class hello{
+public static void main(String args[]){
+int[] arr={1,4,6,7,9,3,4};
+Arrays.sort(arr);
+for(int i=0;i<arr.length;i++){
+System.out.println(arr[i]);
+}
+}
+}
