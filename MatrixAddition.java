c5afbcf31534b8a60ffeece08e23a2fe46deb6ec
Jyothika Karanam <jyothikakaranam991@gmail.com>
2025-08-06 18:16:16 +0530

    Add files via upload

---
+++ MatrixAddition.java
@@ -0,0 +1,35 @@
+public class MatrixAddition {
+    public static void main(String[] args) {
+        // Define two 2D arrays (matrices)
+        int[][] matrixA = {
+            {1, 2, 3},
+            {4, 5, 6},
+            {7, 8, 9}
+        };
+
+        int[][] matrixB = {
+            {9, 8, 7},
+            {6, 5, 4},
+            {3, 2, 1}
+        };
+
+        // Create a result matrix to store the sum
+        int[][] result = new int[3][3];
+
+        // Perform matrix addition
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                result[i][j] = matrixA[i][j] + matrixB[i][j];
+            }
+        }
+
+        // Print the result matrix
+        System.out.println("Result of Matrix Addition:");
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                System.out.print(result[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
