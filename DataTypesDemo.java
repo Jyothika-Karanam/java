ad66f2f8500f8d83b4f2f2887c86b71f3d95dcd7
Jyothika Karanam <jyothikakaranam991@gmail.com>
2025-08-14 21:00:46 +0530

    Add files via upload

---
+++ DataTypesDemo.java
@@ -0,0 +1,27 @@
+public class DataTypesDemo {
+    public static void main(String[] args) {
+        // Integer type
+        int age = 25;
+
+        // Floating-point types
+        float height = 5.9f;
+        double weight = 70.5;
+
+        // Character type
+        char grade = 'A';
+
+        // Boolean type
+        boolean isStudent = true;
+
+        // String type (not a primitive, but commonly used)
+        String name = "Arjun";
+
+        // Output the values
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Height: " + height + " feet");
+        System.out.println("Weight: " + weight + " kg");
+        System.out.println("Grade: " + grade);
+        System.out.println("Is Student: " + isStudent);
+    }
+}
