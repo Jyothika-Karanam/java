764733e0518db67a6fc1a5a7b314d71995f38592
Jyothika Karanam <jyothikakaranam991@gmail.com>
2025-08-30 21:11:06 +0530

    Add files via upload

---
+++ SeminarWelcome.java
@@ -0,0 +1,18 @@
+public class SeminarWelcome {
+    public static void main(String[] args) {
+        String[] banner = {
+            "****************************************",
+            "*                                      *",
+            "*     🎉 Welcome to Jyothika's Seminar 🎉     *",
+            "*                                      *",
+            "*   Topic: Creative Web Development    *",
+            "*   Featuring: HTML, CSS, Audio & Art  *",
+            "*                                      *",
+            "****************************************"
+        };
+
+        for (String line : banner) {
+            System.out.println(line);
+        }
+    }
+}
