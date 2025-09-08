4f36ad70c2ff5705a989ec07702683eb636a1b11
Jyothika Karanam <jyothikakaranam991@gmail.com>
2025-09-08 21:02:21 +0530

    Add files via upload

---
+++ SeminarIntro.java
@@ -0,0 +1,17 @@
+public class SeminarIntro {
+    public static void main(String[] args) {
+        // Bold and expressive welcome message
+        System.out.println("************************************");
+        System.out.println("*  Welcome to Jyothika's Seminar!  *");
+        System.out.println("*  Let's explore Java together 🎉  *");
+        System.out.println("************************************");
+
+        // A little interactive flavor
+        String topic = "Java Basics";
+        int tagsCovered = 5;
+
+        System.out.println("\nToday's Topic: " + topic);
+        System.out.println("Number of tags we'll cover: " + tagsCovered);
+        System.out.println("Ready to dive in? Let's go! 🚀");
+    }
+}
