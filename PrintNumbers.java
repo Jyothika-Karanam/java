298961fbd5958cd5e877c2bfd9d2ebd7c724a43f
Jyothika Karanam <jyothikakaranam991@gmail.com>
2025-09-20 20:58:02 +0530

    Add files via upload

---
+++ PrintNumbers.java
@@ -0,0 +1,8 @@
+public class PrintNumbers {
+    public static void main(String[] args) {
+        System.out.println("🎯 Printing numbers from 1 to 10:");
+        for (int i = 1; i <= 10; i++) {
+            System.out.println("🔢 Number: " + i);
+        }
+    }
+}
