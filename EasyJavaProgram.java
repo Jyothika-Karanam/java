e57d21bd473c3f931ccb3e23212ea39d4f52e761
Jyothika Karanam <jyothikakaranam991@gmail.com>
2025-09-28 14:58:02 +0530

    Add files via upload

---
+++ EasyJavaProgram.java
@@ -0,0 +1,16 @@
+public class EasyJavaProgram {
+    public static void main(String[] args) {
+        // Print a greeting message
+        System.out.println("🌈 Hello, Jyothika! Welcome to Java!");
+
+        // Declare two numbers
+        int num1 = 7;
+        int num2 = 5;
+
+        // Add the numbers
+        int sum = num1 + num2;
+
+        // Print the result
+        System.out.println("✨ The sum of " + num1 + " and " + num2 + " is: " + sum);
+    }
+}
